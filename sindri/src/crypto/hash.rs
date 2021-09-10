2a0ce54af68dc008b4bc606f06e4a32269842d95
Norbert Fabritius <norbert.fabritius@esrlabs.com>
2021-09-10 09:45:10 +0200

    Address code review
    Use digest method instead of init-update-final API.
    Use generic type parameter instead of anonymous type.
    Return digest instead of taking output parameter.
    Avoid copy of result.
    Remove assert and use expect() instead.

--- sindri/src/crypto/hash.rs
+++ sindri/src/crypto/hash.rs
@@ -1,31 +1,29 @@
+use core::convert::TryInto;
 use sha2::{Digest, Sha256, Sha384, Sha512};
 
 pub const SHA256_SIZE: usize = 32;
 pub const SHA384_SIZE: usize = 48;
 pub const SHA512_SIZE: usize = 64;
 
-pub fn sha256(input: impl AsRef<[u8]>, output: &mut [u8; SHA256_SIZE]) {
-    let mut hasher = Sha256::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    assert_eq!(result.len(), SHA256_SIZE);
-    output.copy_from_slice(&result);
+pub fn sha256<T: AsRef<[u8]>>(input: T) -> [u8; SHA256_SIZE] {
+    Sha256::digest(input.as_ref())
+        .as_slice()
+        .try_into()
+        .expect("Unexpected hash output size")
 }
 
-pub fn sha384(input: impl AsRef<[u8]>, output: &mut [u8; SHA384_SIZE]) {
-    let mut hasher = Sha384::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    assert_eq!(result.len(), SHA384_SIZE);
-    output.copy_from_slice(&result);
+pub fn sha384<T: AsRef<[u8]>>(input: T) -> [u8; SHA384_SIZE] {
+    Sha384::digest(input.as_ref())
+        .as_slice()
+        .try_into()
+        .expect("Unexpected hash output size")
 }
 
-pub fn sha512(input: impl AsRef<[u8]>, output: &mut [u8; SHA512_SIZE]) {
-    let mut hasher = Sha512::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    assert_eq!(result.len(), SHA512_SIZE);
-    output.copy_from_slice(&result);
+pub fn sha512<T: AsRef<[u8]>>(input: T) -> [u8; SHA512_SIZE] {
+    Sha512::digest(input.as_ref())
+        .as_slice()
+        .try_into()
+        .expect("Unexpected hash output size")
 }
 
 #[cfg(test)]
@@ -33,8 +31,7 @@ const HELLO_WORLD: &[u8] = b"Hello, World!";
 
 #[test]
 fn test_sha256() {
-    let mut output = [0; SHA256_SIZE];
-    sha256(HELLO_WORLD, &mut output);
+    let output = sha256(HELLO_WORLD);
     let expected = hex::decode("dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f")
         .expect("Failed to decode hex string");
     assert_eq!(output, expected.as_slice());
@@ -42,16 +39,14 @@ fn test_sha256() {
 
 #[test]
 fn test_sha384() {
-    let mut output = [0; SHA384_SIZE];
-    sha384(HELLO_WORLD, &mut output);
+    let output = sha384(HELLO_WORLD);
     let expected = hex::decode("5485cc9b3365b4305dfb4e8337e0a598a574f8242bf17289e0dd6c20a3cd44a089de16ab4ab308f63e44b1170eb5f515").expect("Failed to decode hex string");
     assert_eq!(output, expected.as_slice());
 }
 
 #[test]
 fn test_sha512() {
-    let mut output = [0; SHA512_SIZE];
-    sha512(HELLO_WORLD, &mut output);
+    let output = sha512(HELLO_WORLD);
     let expected = hex::decode("374d794a95cdcfd8b35993185fef9ba368f160d8daf432d08ba9f1ed1e5abe6cc69291e0fa2fe0006a52570ef18c19def4e617c33ce52ef0a6e5fbe318cb0387").expect("Failed to decode hex string");
     assert_eq!(output, expected.as_slice());
 }
