51d9934890b82123798db7aeb7dedc81980fe95a
Norbert Fabritius <norbert.fabritius@esrlabs.com>
2021-09-27 15:36:55 +0200

    Move ECC key generation to ecc.rs
    Get rid of EphemeralSecret in ecdh.rs

--- sindri/src/crypto/ecdh.rs
+++ sindri/src/crypto/ecdh.rs
@@ -1,48 +1,24 @@
-use elliptic_curve::ecdh::{EphemeralSecret, SharedSecret};
+use ecdsa::elliptic_curve::SecretKey;
+use elliptic_curve::ecdh::{diffie_hellman, SharedSecret};
 use elliptic_curve::weierstrass::Curve;
 use elliptic_curve::zeroize::Zeroize;
 use elliptic_curve::{AffinePoint, ProjectiveArithmetic, PublicKey, Scalar};
-use rand::{CryptoRng, RngCore};
 
-#[derive(Debug)]
-pub enum Error {
-    KeyGeneration,
-}
-
-/// Generate a public-private key pair for an ECDH key exchange.   
-///
-/// # Arguments
-///
-/// * `rng`: Random number generator to use for key generation.
-pub fn gen_key_pair<R, C>(rng: &mut R) -> Result<(PublicKey<C>, EphemeralSecret<C>), Error>
-where
-    R: CryptoRng + RngCore,
-    C: Curve + ProjectiveArithmetic,
-    AffinePoint<C>: Zeroize,
-    Scalar<C>: Zeroize,
-    SharedSecret<C>: for<'a> From<&'a AffinePoint<C>>,
-{
-    let private = EphemeralSecret::random(rng);
-    let public = private.public_key();
-    Ok((public, private))
-}
+pub use crate::crypto::ecc::gen_key_pair;
 
 /// Derive a shared secret from a private key and a public key.
 /// If another peer wants to derive the same secret, he has to switch out the keys with their
 /// respective partner keys.
 /// That is, if two peers called A and B have generated their respective key pairs, it holds that
 ///     derive_shared_secret(private_A, public_B) == derive_shared_secret(private_B, public_A)
-pub fn derive_shared_secret<C>(
-    private: &EphemeralSecret<C>,
-    public: &PublicKey<C>,
-) -> SharedSecret<C>
+pub fn derive_shared_secret<C>(private: &SecretKey<C>, public: &PublicKey<C>) -> SharedSecret<C>
 where
     C: Curve + ProjectiveArithmetic,
     AffinePoint<C>: Zeroize,
     Scalar<C>: Zeroize,
     SharedSecret<C>: for<'a> From<&'a AffinePoint<C>>,
 {
-    private.diffie_hellman(public)
+    diffie_hellman(private.to_secret_scalar(), public.as_affine())
 }
 
 #[cfg(test)]
@@ -55,10 +31,8 @@ mod test {
     fn test_p256() {
         let entropy = rng::test::TestEntropySource::default();
         let mut rng = rng::Rng::new(entropy, None);
-        let (local_public, local_private) =
-            gen_key_pair::<_, NistP256>(&mut rng).expect("Failed to generate local key pair");
-        let (remote_public, remote_private) =
-            gen_key_pair::<_, NistP256>(&mut rng).expect("Failed to generate remote key pair");
+        let (local_public, local_private) = gen_key_pair::<_, NistP256>(&mut rng);
+        let (remote_public, remote_private) = gen_key_pair::<_, NistP256>(&mut rng);
         let local_secret = derive_shared_secret(&local_private, &remote_public);
         let remote_secret = derive_shared_secret(&remote_private, &local_public);
         assert_eq!(local_secret.as_bytes(), remote_secret.as_bytes());
