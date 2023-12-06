0a8a893f802ddc8dbffb45ff60de0576dd68b91b
Ruslan Piasetskyi <ruslan.piasetskyi@gmail.com>
2023-12-06 14:48:52 +0100

    Move ECC key pair generation from worker to crypto module

--- heimlig/src/hsm/workers/ecc_worker.rs
+++ heimlig/src/hsm/workers/ecc_worker.rs
@@ -1,22 +1,18 @@
 use crate::common::jobs::{ClientId, Error, Request, RequestId, Response};
 use crate::crypto;
-use crate::crypto::ecc::generate_key_pair;
 use crate::crypto::ecdsa::{
-    nist_p256_sign, nist_p256_sign_prehashed, nist_p256_verify, nist_p256_verify_prehashed,
-    nist_p384_sign, nist_p384_sign_prehashed, nist_p384_verify, nist_p384_verify_prehashed,
+    nist_p256_generate_key_pair, nist_p256_sign, nist_p256_sign_prehashed, nist_p256_verify,
+    nist_p256_verify_prehashed, nist_p384_generate_key_pair, nist_p384_sign,
+    nist_p384_sign_prehashed, nist_p384_verify, nist_p384_verify_prehashed,
 };
 use crate::crypto::rng::{EntropySource, Rng};
 use crate::hsm::keystore;
 use crate::hsm::keystore::{KeyId, KeyInfo, KeyStore, KeyType};
 use core::ops::{Deref, DerefMut};
-use elliptic_curve::sec1::{FromEncodedPoint, ModulusSize, ToEncodedPoint};
-use elliptic_curve::{AffinePoint, CurveArithmetic, FieldBytesSize};
 use embassy_sync::blocking_mutex::raw::RawMutex;
-use embassy_sync::mutex::{Mutex, MutexGuard};
+use embassy_sync::mutex::Mutex;
 use futures::{Sink, SinkExt, Stream, StreamExt};
-use p256::NistP256;
-use p384::NistP384;
-use zeroize::Zeroizing;
+use zeroize::{Zeroize, Zeroizing};
 
 pub struct EccWorker<
     'data,
@@ -129,85 +125,57 @@ impl<
         key_id: KeyId,
         overwrite: bool,
     ) -> Response<'data> {
-        let locked_key_store = self.key_store.lock().await;
+        let mut locked_key_store = self.key_store.lock().await;
         let key_info = locked_key_store.deref().get_key_info(key_id);
-        match key_info {
-            Err(e) => Response::Error {
-                client_id,
-                request_id,
-                error: Error::KeyStore(e),
-            },
-            Ok(key_info) => match key_info.ty {
-                KeyType::EccKeypairNistP256 => {
-                    self.generate_key_pair_internal::<NistP256>(
-                        locked_key_store,
-                        client_id,
-                        request_id,
-                        key_info,
-                        overwrite,
-                    )
-                    .await
-                }
-                KeyType::EccKeypairNistP384 => {
-                    self.generate_key_pair_internal::<NistP384>(
-                        locked_key_store,
-                        client_id,
-                        request_id,
-                        key_info,
-                        overwrite,
-                    )
-                    .await
-                }
-                _ => Response::Error {
+        let mut private_key_bytes = Zeroizing::new([0u8; KeyType::MAX_PRIVATE_KEY_SIZE]);
+        let mut public_key_bytes = Zeroizing::new([0u8; KeyType::MAX_PUBLIC_KEY_SIZE]);
+
+        let ((private_key, public_key), key_info) = match key_info {
+            Err(e) => {
+                return Response::Error {
                     client_id,
                     request_id,
-                    error: Error::KeyStore(keystore::Error::InvalidKeyType),
-                },
+                    error: Error::KeyStore(e),
+                }
+            }
+            Ok(key_info) => match key_info.ty {
+                KeyType::EccKeypairNistP256 => {
+                    let (private_key, public_key) =
+                        nist_p256_generate_key_pair(self.rng.lock().await.deref_mut());
+                    (
+                        move_key_pair(
+                            private_key,
+                            public_key,
+                            private_key_bytes.as_mut_slice(),
+                            public_key_bytes.as_mut_slice(),
+                        ),
+                        key_info,
+                    )
+                }
+                KeyType::EccKeypairNistP384 => {
+                    let (private_key, public_key) =
+                        nist_p384_generate_key_pair(self.rng.lock().await.deref_mut());
+                    (
+                        move_key_pair(
+                            private_key,
+                            public_key,
+                            private_key_bytes.as_mut_slice(),
+                            public_key_bytes.as_mut_slice(),
+                        ),
+                        key_info,
+                    )
+                }
+                _ => {
+                    return Response::Error {
+                        client_id,
+                        request_id,
+                        error: Error::KeyStore(keystore::Error::InvalidKeyType),
+                    }
+                }
             },
-        }
-    }
+        };
 
-    async fn generate_key_pair_internal<C>(
-        &mut self,
-        mut locked_key_store: MutexGuard<'_, M, &mut (dyn KeyStore + Send)>,
-        client_id: ClientId,
-        request_id: RequestId,
-        key_info: KeyInfo,
-        overwrite: bool,
-    ) -> Response<'data>
-    where
-        C: CurveArithmetic,
-        AffinePoint<C>: FromEncodedPoint<C> + ToEncodedPoint<C>,
-        FieldBytesSize<C>: ModulusSize,
-    {
-        let (public_key, private_key) =
-            generate_key_pair::<_, C>(self.rng.lock().await.deref_mut());
-
-        // Write public key
-        let mut public_key_bytes = [0u8; KeyType::MAX_PUBLIC_KEY_SIZE];
-        let public_key_bytes = &mut public_key_bytes[..key_info.ty.public_key_size()];
-        let public_key_encoded = &public_key.to_encoded_point(false);
-        assert!(
-            !public_key_encoded.is_compressed()
-                && !public_key_encoded.is_compact()
-                && !public_key_encoded.is_identity(),
-            "expected uncompressed public key point"
-        );
-        let sec1_bytes = public_key_encoded.as_bytes();
-        assert_eq!(sec1_bytes.len(), 1 + key_info.ty.public_key_size()); // Includes 0x04 prefix
-        public_key_bytes.copy_from_slice(&sec1_bytes[1..]); // Skip prefix
-
-        // Write private key
-        let private_key_bytes = private_key.to_bytes();
-        assert_eq!(private_key_bytes.len(), key_info.ty.private_key_size());
-
-        // Import key pair into key store
-        match locked_key_store.import_key_pair(
-            key_info.id,
-            public_key_bytes,
-            private_key_bytes.as_slice(),
-            overwrite,
-        ) {
+        match locked_key_store.import_key_pair(key_info.id, public_key, private_key, overwrite) {
             Ok(()) => Response::GenerateKeyPair {
                 client_id,
                 request_id,
@@ -473,4 +441,21 @@ impl<
             locked_key_store.get_key_info(key_id)?,
         ))
     }
+}
+
+fn move_key_pair<'a, const N: usize, const M: usize>(
+    mut private_key: [u8; N],
+    mut public_key: [u8; M],
+    private_key_bytes: &'a mut [u8],
+    public_key_bytes: &'a mut [u8],
+) -> (&'a [u8], &'a [u8]) {
+    let private_key_result = &mut private_key_bytes[..private_key.len()];
+    private_key_result.copy_from_slice(&private_key);
+    let public_key_result = &mut public_key_bytes[..public_key.len()];
+    public_key_result.copy_from_slice(&public_key);
+
+    private_key.zeroize();
+    public_key.zeroize();
+
+    (private_key_result, public_key_result)
 }
