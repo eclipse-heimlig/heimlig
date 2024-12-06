74f717c5896ae140b04a52baf96f6eaa6ead8496
Norbert Fabritius <norbert.fabritius@accenture.com>
2024-12-06 11:58:54 +0100

    Remove barely used limits.rs
    Imposing limits on buffer sizes is part of the job of the integrator.
    Heimlig has no way of assuming sane defaults.

--- heimlig/src/common/limits.rs
+++
@@ -1,8 +0,0 @@
-/// Maximum number of random bytes that can be requested at once.
-pub const MAX_RANDOM_SIZE: usize = 1500; // Ethernet max. MTU size
-
-/// Maximum plaintext length for symmetric encryption.
-pub const MAX_PLAINTEXT_SIZE: usize = 1500; // Ethernet max. MTU size
-
-/// Maximum ciphertext length for symmetric encryption.
-pub const MAX_CIPHERTEXT_SIZE: usize = 1500; // Ethernet max. MTU size
